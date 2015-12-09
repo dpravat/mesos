6f7d5a8a58331b399908ffe1b1029b2fb5b87282
Alex Naparu <alex.naparu@microsoft.com>
2015-12-09 15:02:23 -0800

    Windows: Forked permissions.hpp and added no-op Windows implementation.

--- 3rdparty/libprocess/3rdparty/stout/include/stout/os.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/os.hpp
@@ -51,6 +51,7 @@
 #include <stout/os/mkdir.hpp>
 #include <stout/os/mkdtemp.hpp>
 #include <stout/os/mktemp.hpp>
+#include <stout/os/permissions.hpp>
 #include <stout/os/process.hpp>
 #include <stout/os/rename.hpp>
 #include <stout/os/rm.hpp>
