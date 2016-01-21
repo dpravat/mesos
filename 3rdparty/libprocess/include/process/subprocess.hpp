cd05d133d072007e5b5f620c24b3d51e5daf92aa
Alex Naparu <alex.naparu@microsoft.com>
2016-01-20 19:03:14 -0800

    Windows: Forked subprocess.cpp, added `Windows` implementation.
    Review: https://reviews.apache.org/r/44141

--- 3rdparty/libprocess/include/process/subprocess.hpp
+++ 3rdparty/libprocess/include/process/subprocess.hpp
@@ -74,8 +74,13 @@ public:
      */
     struct InputFileDescriptors
     {
+#ifndef __WINDOWS__
       int read = -1;
       Option<int> write = None();
+#else
+      HANDLE read = INVALID_HANDLE_VALUE;
+      Option<HANDLE> write = None();
+#endif // __WINDOWS__
     };
 
     /**
@@ -89,8 +94,13 @@ public:
      */
     struct OutputFileDescriptors
     {
+#ifndef __WINDOWS__
       Option<int> read = None();
       int write = -1;
+#else
+      Option<HANDLE> read = None();
+      HANDLE write = INVALID_HANDLE_VALUE;
+#endif // __WINDOWS__
     };
 
     /**
