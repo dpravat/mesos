b65456a0dc5b2c01a90bf0927164e96b8bd70710
Alex Clemmer <clemmer.alexander@gmail.com>
2016-02-01 10:20:15 -0800

    SASL be gone.
    Review: https://reviews.apache.org/r/42945

--- src/master/master.cpp
+++ src/master/master.cpp
@@ -417,6 +417,7 @@ void Master::initialize()
             << "Must be within [0%-100%]";
   }
 
+#ifdef HAS_AUTHENTICATION
   // Log authentication state.
   if (flags.authenticate_frameworks) {
     LOG(INFO) << "Master only allowing authenticated frameworks to register";
@@ -428,6 +429,7 @@ void Master::initialize()
   } else {
     LOG(INFO) << "Master allowing unauthenticated slaves to register";
   }
+#endif
 
   // Load credentials.
   Option<Credentials> credentials;
@@ -444,6 +446,7 @@ void Master::initialize()
     credentials = _credentials.get();
   }
 
+#ifdef HAS_AUTHENTICATION
   // Extract authenticator names and validate them.
   authenticatorNames = strings::split(flags.authenticators, ",");
   if (authenticatorNames.empty()) {
@@ -499,6 +502,7 @@ void Master::initialize()
       authenticator = None();
     }
   }
+#endif
 
   // TODO(arojas): Consider creating a factory function for the instantiation
   // of the HTTP authenticator the same way the allocator does.
