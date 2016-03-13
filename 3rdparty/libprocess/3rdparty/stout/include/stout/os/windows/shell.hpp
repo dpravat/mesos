a98d15b1a9c5aa5dd6c259387b9229a7e813f9ac
Daniel Pravat <dpravat@outlook.com>
2016-03-12 23:12:34 -0800

    Windows: Added Windows shell names.

--- 3rdparty/libprocess/3rdparty/stout/include/stout/os/windows/shell.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/os/windows/shell.hpp
@@ -31,9 +31,9 @@ namespace Shell {
 // by the callee, usualy the command name and arg1() is the second
 // command argument received by the callee.
 
-  constexpr const char* name = "sh";
-  constexpr const char* arg0 = "sh";
-  constexpr const char* arg1 = "-c";
+  constexpr const char* name = "cmd.exe";
+  constexpr const char* arg0 = "cmd";
+  constexpr const char* arg1 = "/c";
 } // namespace Shell {
 
 // Runs a shell command formatted with varargs and return the return value
