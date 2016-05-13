51f1d0e93f43417fdcb8257ae9a626a353904efa
Alex Clemmer <clemmer.alexander@gmail.com>
2016-05-13 10:09:11 -0700

    Stout: transitioned Stout to use `HANDLE` io APIs on Windows.

--- 3rdparty/stout/include/stout/protobuf.hpp
+++ 3rdparty/stout/include/stout/protobuf.hpp
@@ -84,6 +84,16 @@ inline Try<Nothing> write(int fd, const google::protobuf::Message& message)
 }
 
 
+#ifdef __WINDOWS__
+// NOTE: Ordinarily this would go in a Windows-specific header; we put it here
+// to avoid complex forward declarations.
+inline Try<Nothing> write(HANDLE handle, const google::protobuf::Message& message)
+{
+  return write(_open_osfhandle(reinterpret_cast<intptr_t>(handle), O_WRONLY), message);
+}
+#endif // __WINDOWS__
+
+
 // Write out the given sequence of protobuf messages to the
 // specified file descriptor by repeatedly invoking write
 // on each of the messages.
@@ -291,6 +301,20 @@ Result<T> read(int fd, bool ignorePartial = false, bool undoFailed = false)
 {
   return internal::Read<T>()(fd, ignorePartial, undoFailed);
 }
+
+
+#ifdef __WINDOWS__
+// NOTE: Ordinarily this would go in a Windows-specific header; we put it here
+// to avoid complex forward declarations.
+template <typename T>
+Result<T> read(HANDLE handle, bool ignorePartial = false, bool undoFailed = false)
+{
+  return read<T>(
+      _open_osfhandle(reinterpret_cast<intptr_t>(handle), O_RDONLY),
+      ignorePartial,
+      undoFailed);
+}
+#endif // __WINDOWS__
 
 
 // A wrapper function that wraps the above read() with open and
