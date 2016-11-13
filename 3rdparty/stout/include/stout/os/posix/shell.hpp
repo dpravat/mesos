fadde2d2d8614402bd178624c6fa086d378ecfa7
Daniel Pravat <dpravat@outlook.com>
2016-11-13 05:17:16 +0000

    Define Entrypoint.

--- 3rdparty/stout/include/stout/os/posix/shell.hpp
+++ 3rdparty/stout/include/stout/os/posix/shell.hpp
@@ -38,6 +38,7 @@ namespace Shell {
 // received by the callee, usually the command name and `arg1` is the
 // second command argument received by the callee.
 
+constexpr const char* entrypoint = "/bin/sh";
 constexpr const char* name = "sh";
 constexpr const char* arg0 = "sh";
 constexpr const char* arg1 = "-c";
